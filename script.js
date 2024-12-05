4d9e80cb0f298759aa84750b689c0c06b0bdf512
aleksandardevelops <119680237+aleksandardevelops@users.noreply.github.com>
2024-12-05 01:29:20 +0100

    Initialize player score variables with 0 and add playRound function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-  console.log(randomNumber);
+let humanScore = 0;
+let computerScore = 0;
 
-  if (randomNumber === 1) {
+function getComputerChoice() {
+  let computerChoice = Math.floor(Math.random() * 3) + 1;
+  console.log(computerChoice);
+
+  if (computerChoice === 1) {
     console.log('Computer: Rock');
-  } else if (randomNumber === 2) {
+  } else if (computerChoice === 2) {
     console.log('Computer: Papper');
   } else {
     console.log('Computer: Scissors');
@@ -12,3 +15,11 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+function getHumanChoice() {
+  let humanChoice = prompt('Enter your choice');
+}
+
+getHumanChoice();
+
+function playRound() {}
